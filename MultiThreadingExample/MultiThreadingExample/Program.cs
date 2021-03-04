6420ef0c3a74c4d63c8610e00f8a94d29eb21c26
Aladin <riabi.aladin@gmail.com>
2021-03-04 09:52:29 -0400

    MultiThreadingExample

---
+++ MultiThreadingExample/MultiThreadingExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Threading;
+
+namespace MultiThreadingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread T = new Thread(Print1);
+
+            T.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(Thread.GetCurrentProcessorId());
+            }
+            Console.ReadKey();
+        }
+
+        static void Print1()
+        {
+            for (int i=0; i<100; i++)
+            {
+                Console.WriteLine(Thread.GetCurrentProcessorId());
+            }
+            
+        }
+    }
+}
